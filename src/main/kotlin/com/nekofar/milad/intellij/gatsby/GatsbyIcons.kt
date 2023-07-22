74484e0b178514894546650ce5c1670224bc2be0
Milad Nekofar <milad@nekofar.com>
2023-07-22 12:56:05 +0330

    chore: simplify SVG code and remove unused Icon
    Simplified the code for SVG files (type.svg, gatsby.svg, pluginIcon.svg) by reducing unnecessary metadata and consolidating the paths for the shapes. The 'FileType' field in GatsbyIcons.kt was also removed because it was fetching the 'type.svg' icon which is no longer in use. This cleanup process helps improve the performance and readability of the code.

--- src/main/kotlin/com/nekofar/milad/intellij/gatsby/GatsbyIcons.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/gatsby/GatsbyIcons.kt
@@ -4,8 +4,5 @@ import com.intellij.openapi.util.IconLoader
 
 object GatsbyIcons {
     @JvmField
-    val FileType = IconLoader.getIcon("/icons/type.svg", javaClass)
-
-    @JvmField
     val ProjectGenerator = IconLoader.getIcon("/icons/gatsby.svg", javaClass)
 }
