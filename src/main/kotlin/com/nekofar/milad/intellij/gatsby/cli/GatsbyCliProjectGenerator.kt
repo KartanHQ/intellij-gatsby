53d3d8d00afa5bd1ee5af4427bf139f3af92b96a
Milad Nekofar <milad@nekofar.com>
2023-07-22 13:29:28 +0330

    refactor: update project generator for code style consistency
    A code refactor was performed on the GatsbyCliProjectGenerator class for the sake of code style consistency. Mainly, the usage of ":" in function declarations has been omitted when it can be inferred by the compiler. Moreover, instead of directly importing and calling GatsbyBundle.message(), a top-level import operation is done for GatsbyBundle.message and it is called as a function for better readability. These changes aim to improve code readability without modifying any functionality or logic.

--- src/main/kotlin/com/nekofar/milad/intellij/gatsby/cli/GatsbyCliProjectGenerator.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/gatsby/cli/GatsbyCliProjectGenerator.kt
@@ -6,34 +6,31 @@ import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.vfs.VirtualFile
-import com.nekofar.milad.intellij.gatsby.GatsbyBundle
+import com.nekofar.milad.intellij.gatsby.GatsbyBundle.message
 import com.nekofar.milad.intellij.gatsby.GatsbyIcons
-import javax.swing.Icon
 
 class GatsbyCliProjectGenerator : NpmPackageProjectGenerator() {
     private data class PackageInfo(val name: String, val executable: String, val command: String)
 
     private val packageInfo = PackageInfo("gatsby", "gatsby", "new")
 
-    override fun getName(): String = GatsbyBundle.message("gatsby.project.generator.name")
+    override fun getName() = message("gatsby.project.generator.name")
 
-    override fun getDescription(): String = GatsbyBundle.message("gatsby.project.generator.description")
+    override fun getDescription() = message("gatsby.project.generator.description")
 
-    override fun filters(project: Project, baseDir: VirtualFile): Array<Filter> = emptyArray()
+    override fun filters(project: Project, baseDir: VirtualFile) = emptyArray<Filter>()
 
     override fun customizeModule(virtualFile: VirtualFile, contentEntry: ContentEntry?) {}
 
-    override fun packageName(): String = packageInfo.name
+    override fun packageName() = packageInfo.name
 
-    override fun presentablePackageName(): String =
-        GatsbyBundle.message("gatsby.project.generator.presentable.package.name")
+    override fun presentablePackageName() = message("gatsby.project.generator.presentable.package.name")
 
     override fun getNpxCommands() = listOf(NpxPackageDescriptor.NpxCommand(packageInfo.name, packageInfo.executable))
 
-    override fun generateInTemp(): Boolean = true
+    override fun generateInTemp() = true
 
-    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?): Array<String> =
-        arrayOf(packageInfo.command, project?.name.toString())
+    override fun generatorArgs(project: Project, baseDir: VirtualFile) = arrayOf(packageInfo.command, project.name)
 
-    override fun getIcon(): Icon = GatsbyIcons.ProjectGenerator
+    override fun getIcon() = GatsbyIcons.ProjectGenerator
 }
